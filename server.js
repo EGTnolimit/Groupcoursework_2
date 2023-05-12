2c39389d93c87e10c90d48adff3f66939dda5c76
Emmanuel <etgamelord@gmail.com>
2023-05-12 17:53:58 +0400

    Added-get-request

--- server.js
+++ server.js
@@ -8,3 +8,12 @@ const lessons = [
   { topic: 'math', location: 'Brent Cross', price: 90 },
   { topic: 'math', location: 'Golders Green', price: 120 }
 ];
+
+app.get('/lessons', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.json(lessons);
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}/lessons`);
+  });
