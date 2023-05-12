eead66f1cc2b3090e75b2624e961ccaefff8dfb0
Emmanuel <etgamelord@gmail.com>
2023-05-12 17:52:48 +0400

    Added Lessons

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const lessons = [
+  { topic: 'math', location: 'Hendon', price: 100 },
+  { topic: 'math', location: 'Colindale', price: 80 },
+  { topic: 'math', location: 'Brent Cross', price: 90 },
+  { topic: 'math', location: 'Golders Green', price: 120 }
+];
